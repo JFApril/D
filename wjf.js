f84a1c22bf5cdb4b567f576998e6b99e61526c84
JFApril <97507987+JFApril@users.noreply.github.com>
2022-07-18 17:53:17 +0800

    Update wjf.js

--- wjf.js
+++ wjf.js
@@ -1,1 +1,10 @@
-
+var a=document.getElementsByName("details-control")
+for(i=0;i<a.length;i++){
+a[i].click()
+}
+var b=document.getElementsByName("verakName")
+var j=1
+do{
+    b[j].value="Wang Jianfeng"  
+    j+=4
+}while(j<b.length)
