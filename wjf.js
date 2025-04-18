7a0100eb40fdd1c416a1e5285eed05066455fa3c
JFApril <97507987+JFApril@users.noreply.github.com>
2025-04-18 09:32:17 +0800

    更新 wjf.js

--- wjf.js
+++ wjf.js
@@ -8,3 +8,24 @@ do{
     b[j].value="Wang Jianfeng"  
     j+=4
 }while(j<b.length)
+
+
+
+var widget = id("com.taobao.taobao:id/count_down_timer_view_container").findOne(200);
+if (widget) {
+    // 长按控件的中心点
+    longClick(widget.bounds().centerX(), widget.bounds().centerY());
+    sleep(500); // 等待200毫秒
+    
+    // 获取控件左上角的坐标
+    var px1 = widget.bounds().left;
+    var py1 = widget.bounds().top;
+    
+    // 点击相对于控件左上角偏移的坐标
+    click(px1 + 100, py1 - 244);
+    sleep(200);
+    id("com.taobao.taobao:id/uik_mdButtonDefaultPositive").findOne(200).click();
+    toastLog("已删除聊天页喷倒计时");
+} else {
+    toastLog("未找到聊天页喷倒计时");  
+}
